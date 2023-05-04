8e379a9dd5fe04b0d886b91c3f942a545c0ecefa
Toure Dunnon <tdunnon@gmail.com>
2023-05-04 08:19:37 -0400

    Added: Example rust match expression.
    This change includes a simple pattern match expression for the purpose
    of a rust language tutorial.
    
    Signed-off-by: Toure Dunnon <tdunnon@gmail.com>

--- examples/10-match-control-flow/main.rs
+++ examples/10-match-control-flow/main.rs
@@ -1,1 +1,14 @@
-fn main() {}
+// Match control flow
+//
+//
+fn main() {
+    let name = "Hello";
+
+    // use of match expression to pattern match against varible "name"
+    match name {
+        "Good Bye" => println!("Sorry to see you go."),
+        "Hello" => println!("Hi, nice to meet!"),
+        _ => println!("I can't find a greating, good bye."),
+    }
+
+}
