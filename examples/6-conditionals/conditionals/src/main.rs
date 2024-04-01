a8c184a538f5b869b3f303f45628458e2530c83f
Michael Sweeney <87456288+sweeml@users.noreply.github.com>
2024-04-01 21:24:25 +0000

    Added a conditional statement to answer the challenge question.

--- examples/6-conditionals/conditionals/src/main.rs
+++ examples/6-conditionals/conditionals/src/main.rs
@@ -1,11 +1,14 @@
 // There are other conditionals that we can explore in Rust. Like using `if let`
 
 fn main() {
-    let maybe_number: Option<Option<()>> = None;
-    //let maybe_number = Some(42);
-    if let Some(number) = maybe_number {
+    // let maybe_number: Option<Option<()>> = None;
+    let maybe_number = Some(Some(7654));
+    if let Some(Some(number)) = maybe_number {
         println!("The number is {:?}", number);
+    } else if let Some(Some(42)) = maybe_number {
+        println!("The number is {:?}", 42);
     } else {
         println!("There is no number");
     }
 }
+// Added the else if let block as a way to show that we can have multiple conditions. Was part of the challenge questions
